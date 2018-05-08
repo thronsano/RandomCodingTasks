cf8dd55db219d3496a262d2e9a3c30dc7f1d4ca2
Piotr Gonera <piotrgonera95@gmail.com>
2018-05-08 18:57:53 +0200

    final commit, ready to send to the teacher

--- src/NumberSet.java
+++ src/NumberSet.java
@@ -1,5 +1,9 @@
 import java.util.Random;
 
+/**
+ * Task is to implement methods marked as public based on their provided descriptions in comments. Then add as many asserts as we can.
+ */
+
 public class NumberSet {
     private static int MAX_SIZE = 100;
     private int[] nSet = new int[MAX_SIZE];
@@ -13,12 +17,16 @@ public class NumberSet {
      * @throws Exception występuje w przypadku przepełnienia zbioru
      */
     public void add(int i) throws Exception {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+
         if (size >= MAX_SIZE)
             throw new Exception("Zbiór przepełniony!");
         else {
             nSet[size] = i;
             size++;
         }
+
+        assert contains(i) : "Element nie został dodany!";
     }
 
     /**
@@ -29,16 +37,19 @@ public class NumberSet {
      *                   którą chcemy usunąć
      */
     public void remove(int i) throws Exception {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+
         boolean occurred = false;
 
         for (int currentNo = 0; currentNo < size; currentNo++) {
             if (nSet[currentNo] == i) {
                 shiftListLeft(currentNo);
                 occurred = true;
-
                 currentNo--; //Check new number at this index again
             }
         }
+
+        assert !contains(i) : "Liczba nie została usunięta!";
 
         if (!occurred)
             throw new Exception("Liczba nie wystąpiła!");
@@ -50,10 +61,12 @@ public class NumberSet {
      * @param beginning indeks nadpisywanego elementu
      */
     private void shiftListLeft(int beginning) {
-        for (int i = beginning; i < size - 1; i++)
-            nSet[i] = nSet[i + 1];
+        assert nSet != null : "Tablica niezainicjalizowana!";
+
+        System.arraycopy(nSet, beginning + 1, nSet, beginning, size - 1 - beginning);
 
         size--;
+        assert size >= 0 : "Ilość elemnetów w tablicy jest niepoprawna!";
     }
 
     /**
@@ -63,12 +76,23 @@ public class NumberSet {
      * @throws Exception występuje jeśli zbiór jest pustu
      **/
     public int getRandomValue() throws Exception {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+
         Random random = new Random();
         int randomVal = random.nextInt(size);
 
-        remove(nSet[randomVal]);
+        assert randomVal >= 0 && randomVal < size : "Wylosowany indeks jest niepoprawny!";
 
-        return randomVal;
+        int noToDelete = nSet[randomVal];
+
+        for (int i = 0; i < size; i++) {
+            if (nSet[i] == noToDelete) {
+                shiftListLeft(i);
+                break;
+            }
+        }
+
+        return noToDelete;
     }
 
     /**
@@ -78,13 +102,23 @@ public class NumberSet {
      * @throws Exception występuje jeśli zbiór jest pusty.
      */
     public int getSumOfElements() throws Exception {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+
+        boolean allNoPositive = true;
+
         if (size == 0)
             throw new Exception("Zbiór jest pusty!");
 
         int sum = 0;
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) {
             sum += nSet[i];
+
+            if (nSet[i] < 0)
+                allNoPositive = false;
+        }
+
+        assert !allNoPositive || sum >= 0 : "Suma liczb dodanich jest ujemna!";
 
         return sum;
     }
@@ -94,12 +128,10 @@ public class NumberSet {
      * wartość bez reszty
      *
      * @param d liczba przez którą dzielimy
-     * @throws Exception występuje w przypadku dzielenia przez 0
-     *                   (można zastąpić asercją)
      */
-    public void divideAllElementsBy(int d) throws Exception {
-        if (d == 0)
-            throw new Exception("Nie można podzielić przez zero!");
+    public void divideAllElementsBy(int d) {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+        assert d != 0 : "Nie można podzielić przez 0!";
 
         for (int i = 0; i < size; i++)
             nSet[i] = nSet[i] / d;
@@ -112,6 +144,9 @@ public class NumberSet {
      * @return true w przypadku odnalezienia wartości, false w przeciwnym razie.
      */
     public boolean contains(int i) {
+        assert nSet != null : "Tablica niezainicjalizowana!";
+        assert size != 0 : "Tablica nie zawiera elementów!";
+
         for (int j = 0; j < size; j++)
             if (nSet[j] == i)
                 return true;
@@ -125,28 +160,29 @@ public class NumberSet {
      * @return rozmiar zbioru
      */
     public int getSize() {
+        assert nSet != null : "Tablica niezainicjalizowana!";
         return size;
     }
 
     public static void main(String[] args) {
         NumberSet numberSet = new NumberSet();
+
         try {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 99; i++)
                 numberSet.add(5);
 
+            numberSet.add(6);
             numberSet.remove(5);
-            //numberSet.add(6);
+            numberSet.add(7);
 
-            //numberSet.divideAllElementsBy(2);
+            numberSet.divideAllElementsBy(2);
 
-            //System.out.println("Random val: " + numberSet.getRandomValue());
-            //System.out.println("Contains 5: " + numberSet.contains(5));
+            System.out.println("Random val: " + numberSet.getRandomValue());
+            System.out.println("Contains 5: " + numberSet.contains(5));
             System.out.println("Size: " + numberSet.getSize());
             System.out.println("Sum: " + numberSet.getSumOfElements());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
 }
