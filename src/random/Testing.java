2eb331e3a26663164533d11ddc29fcefc176fca3
Piotr Gonera <piotrgonera95@gmail.com>
2018-06-05 18:20:33 +0200

    added quick fibonacci implementation

---
+++ src/random/Testing.java
@@ -0,0 +1,10 @@
+package random;
+
+public class Testing {
+    public static void main(String[] args) {
+        Testing testing = new Testing();
+
+        String zdanie = "Czesc jestem Ania";
+        System.out.println(zdanie.split("\\s").length);
+    }
+}
